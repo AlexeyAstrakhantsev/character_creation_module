8577a243599b6424f41b07ac22847a3b3404c3c4
Alexey Astrakhantsev <alexey.i.astrakhantsev@gmail.com>
2024-11-09 20:21:20 +0800

    Add working version without classes

--- importmath.py
+++ importmath.py
@@ -1,22 +1,58 @@
-from math import sqrt
-
-message = """Добро пожаловать в самую лучшую программу
-          для вычисления квадратного корня из заданного числа"""
-print(message)
+from datetime import datetime
+import time
 
 
-def CalculateSquareRoot(number: float) -> float:
-    """ Вычисляет квадратный корень"""
-    return sqrt(number)
+class Quest:
+    def __init__(self, name, goal, description):
+        self.name = name
+        self.goal = goal
+        self.description = description
+        self.start_time = None
+        self.end_time = None
+
+    def __str__(self):
+        about_quest = 'Цель квеста ' + self.name + ' - ' + self.goal
+        if self.end_time:
+            return (f'{about_quest} Квест завершён.')
+        if self.start_time:
+            return (f'{about_quest} Квест выполняется.')
+        return (about_quest)
+
+    def accept_quest(self):
+        if self.end_time:
+            return ('С этим испытанием вы уже справились.')
+        else:
+            self.start_time = datetime.now()
+            return (f'Вы взяли квест в {self.start_time}')
+
+    def pass_quest(self):
+        if not self.start_time:
+            return ('Нельзя завершить то, что не имеет начала!')
+        else:
+            self.end_time = datetime.now()
+            completion_time = self.end_time - self.start_time
+            return (
+                f'Квест "{self.name}" окончен.'
+                f'Время выполнения квеста: {completion_time}')
 
 
-def calc(your_number):
-    if your_number <= 0:
-        return
-    squareroot = CalculateSquareRoot(your_number)
-    print(f"Мы вычислили квадратный корень из введённого вами числа. "
-          f"Это будет: {squareroot}")
+# Объявите класс Quest с методами и свойствами.
+# В этих переменных содержатся значения, которые нужно передать
+# в качестве аргументов в экземпляр класса Quest.
+quest_name = 'Сбор пиксельники'
+quest_goal = 'Соберите 12 ягод пиксельники.'
+quest_description = '''
+В древнем лесу Кодоборье растёт ягода "пиксельника".
+Она нужна для приготовления целебных снадобий.
+Соберите 12 ягод пиксельники.'''
+# Создайте экземпляр класса Quest.
+new_quest = Quest(quest_name, quest_goal, quest_description)
+print(new_quest.pass_quest())
+print(new_quest.accept_quest())
+time.sleep(3)
+print(new_quest.pass_quest())
+print(new_quest.accept_quest())
 
 
-print(message)
-calc(25.5)
+# Печатаем объекта класса Quest:
+print(new_quest)
